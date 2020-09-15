bb908a8f5605999f1268b3dbfce8ebd597daf3a7
Angel Harbison <contact@angelharbison.me>
2020-09-14 21:15:43 -0400

    Refactor whitespace.

--- MessageHandler.php
+++ MessageHandler.php
@@ -56,40 +56,40 @@ class MessageHandler implements MessageComponentInterface {
 	 *
 	 * Then, send client any messages that the server may have.
 	 */
-    public function onOpen(ConnectionInterface $client) {
-    	$this->clients[$client->resourceId] = $client;
-    	$client->send(json_encode($this->messages, JSON_FORCE_OBJECT));
-    }
+	public function onOpen(ConnectionInterface $client) {
+		$this->clients[$client->resourceId] = $client;
+		$client->send(json_encode($this->messages, JSON_FORCE_OBJECT));
+	}
 
 	/**
 	 * Each time message is received by server, add message to message queue.
 	 *
 	 * Then, send message to each connected client.
 	 */
-    public function onMessage(ConnectionInterface $from, $messageIn) {
-    	$messageIndex = ++$this->messageCount;
-    	if (count($this->messages) >= self::MESSAGE_LIMIT) {
-    		array_shift($this->messages);
-    	}
-    	$this->messages[$messageIndex] = $messageIn;
-    	foreach($this->clients as $client) {
-            $client->send(
-            	json_encode([$messageIndex => $messageIn], JSON_FORCE_OBJECT)
-            );
-        }
-    }
+	public function onMessage(ConnectionInterface $from, $messageIn) {
+		$messageIndex = ++$this->messageCount;
+		if (count($this->messages) >= self::MESSAGE_LIMIT) {
+			array_shift($this->messages);
+		}
+		$this->messages[$messageIndex] = $messageIn;
+		foreach($this->clients as $client) {
+			$client->send(
+				json_encode([$messageIndex => $messageIn], JSON_FORCE_OBJECT)
+			);
+		}
+	}
 
 	/**
 	 * Each time a client disconnects, remove their connection from collection.
 	 */
-    public function onClose(ConnectionInterface $client) {
-    	unset($this->clients[$client->resourceId]);
-    }
+	public function onClose(ConnectionInterface $client) {
+		unset($this->clients[$client->resourceId]);
+	}
 
 	/**
 	 * Close client connection on error.
 	 */
-    public function onError(ConnectionInterface $client, \Exception $e) {
-    	$client->close();
-    }
+	public function onError(ConnectionInterface $client, \Exception $e) {
+		$client->close();
+	}
 }
